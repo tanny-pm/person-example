04d962bee385007eff4d0e0f0c2d7ac3a0d3a57a
Tanny <ryosuke.tanimura+dev@gmail.com>
2024-01-18 23:38:36 +0900

    add: 11.2.2コードをスタブアウトする

--- test_person.py
+++ test_person.py
@@ -1,11 +1,10 @@
-import Person
+from Person import Person
 
 person_name: str = "Bob"
 person_age: int = 21
 
 
-def test_create_person_with_name_and_age() {
-    person = Person()
+def test_create_person_with_name_and_age():
+    person = Person(person_name, person_age)
     assert person.name == person_name
     assert person.age == person_age
-}
