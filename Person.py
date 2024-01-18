ffe8905307331b65d6d2ba30923cc3ad37850a94
Tanny <ryosuke.tanimura+dev@gmail.com>
2024-01-18 23:42:53 +0900

    add: 成約を導入する

--- Person.py
+++ Person.py
@@ -1,4 +1,7 @@
 class Person:
+    MINIMUM_AGE = 1
+    MAXIMUM_AGE = 200
+
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
--- test_person.py
+++ test_person.py
@@ -8,3 +8,8 @@ def test_create_person_with_name_and_age():
     person = Person(person_name, person_age)
     assert person.name == person_name
     assert person.age == person_age
+
+
+def test_constants():
+    assert Person.MINIMUM_AGE == 1
+    assert Person.MAXIMUM_AGE == 200
